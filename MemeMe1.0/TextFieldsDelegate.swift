6c68fc8b4ee39ff968df47a4e14bc8e9b6a94846
Okoli Chinedu Christian <Okolichukwunedu@gmail.com>
2022-07-03 17:22:59 +0100

    Add files via upload

---
+++ MemeMe1.0/TextFieldsDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  TextFieldsDelegate.swift
+//  MemeMe1.0
+//
+//  Created by user on 02/07/2022.
+//  Copyright © 2022 OkoliChinedu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextFieldsDelegate: NSObject, UITextFieldDelegate {
+    
+    //MARK: Text Field Delegate Methods
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        // Figure out what the new text will be, if we return true
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        return true
+    }
+
+    let orignialText: [String: String] = [
+        "top": "TOP",
+        "bottom": "BOTTOM"
+    ]
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == orignialText["top"] || textField.text == orignialText["bottom"] {
+            textField.text = " "
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+}
+
+
+
